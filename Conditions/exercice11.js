4bcd5b1f579d9344688610f58541b70dddaf1c89
2103milena <2103milena@gmail.com>
2017-10-05 17:54:54 +0200

    Initial commit

---
+++ Conditions/exercice11.js
@@ -0,0 +1,88 @@
+// 
+
+// var y=6
+
+// if (typeof y === "number" && y%2 == 0) {
+
+//     console.log("paran")
+
+// }
+
+//     else {
+//         console.log("nije paran broj")
+
+
+//     }
+
+
+// var a = true ? "one" : "two"
+// console.log (a) 
+  
+// var x = 15
+// if (x%3 == 0 && x%5 == 0){
+//     console.log(x)
+// }
+
+// else {console.log("nije deljiv")}
+
+// // Write a program that compares two numbers and display the larger. Result display in console.
+
+// var x = 7
+// var y = 100
+
+// if (x>y) {
+//     console.log(x)
+// }
+
+//     else {
+//         console.log(y)
+
+//     }
+
+    var x = 5
+    var y = 3
+    var z = 2
+
+
+if(x>y && x>z) 
+
+{
+    
+        console.log(x y z)
+    }
+
+else if(x>y && z>y) {
+
+    console.log(x z y)
+}
+
+else if(y>x && x>z)
+
+{
+    
+        console.log(y x z )
+    }
+
+else if (y>x && z>x)
+
+{
+    
+        console.log(y z x)
+    }
+
+else if (z>x && x>y)
+
+{
+    
+        console.log(z x y)
+    }
+
+else (z>y && y>x)
+
+{
+    
+        console.log(z y x)
+    }
+
+
+
