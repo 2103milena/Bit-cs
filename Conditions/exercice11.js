69d08cf3c03f90fa1e0b2cd559e9c328f9b615f7
2103milena <2103milena@gmail.com>
2017-10-09 18:12:10 +0200

    vezbe sa for

--- Conditions/exercice11.js
+++ Conditions/exercice11.js
@@ -17,7 +17,7 @@
 
 // var a = true ? "one" : "two"
 // console.log (a) 
-  
+
 // var x = 15
 // if (x%3 == 0 && x%5 == 0){
 //     console.log(x)
@@ -47,7 +47,7 @@
 // if(x>y && x>z) 
 
 // {
-    
+
 //         console.log(x y z)
 //     }
 
@@ -59,58 +59,116 @@
 // else if(y>x && x>z)
 
 // {
-    
+
 //         console.log(y x z )
 //     }
 
 // else if (y>x && z>x)
 
 // {
-    
+
 //         console.log(y z x)
 //     }
 
 // else if (z>x && x>y)
 
 // {
-    
+
 //         console.log(z x y)
 //     }
 
 // else (z>y && y>x)
 
 // {
-    
+
 //         console.log(z y x)
 //     }
 
 
-var day = 4;
+// var day = 4;
 
-switch (day) {
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday");
-        break;
-    case 3:
-        console.log("Wednesday");
-        break;
-    case 4:
-        console.log("Thursday");
-        break;
-    case 5:
-        console.log("Friday");
-        break;
-    case 6:
-        console.log("Saturday");
-        break;
-    case 7:
-        console.log("Sunday");
-        break;
-    default:
-        console.log("Nothing");
-        break;
-}
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Nothing");
+//         break;
+// }
+
+// var day=1;
+
+// switch (day){
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log("It is weekday");
+//     break;    
+//     case 6:
+//     case 7:
+//         console.log("It is weekend");
+//     break;
+//     default:
+//         console.log("Input must be btw 1 and 7");
+//     break;            
+// }
+
+// var ocena = "s";
+// switch (ocena) {
+//     case "A":
+//         console.log("Good job");
+//         break;
+//     case "B":
+//         console.log("Pretty good");
+//         break;
+//     case "C":
+//         console.log("Passed");
+//         break;
+//     case "D":
+//         console.log("Not so good");
+//         break;
+//     case "F":
+//         console.log("Failed");
+//         break;
+//     default:
+//         console.log("Unknown grade");
+//         break;
+// }
+
+
+
+// var months =["January", "February","March","April","May","June","July","August","September","October","November","December"];
+
+// console.log(months[0]);
+// console.log(months[5]);
+// console.log(months[9]);
+
+
+
+var x =[[1,2,1,24],[8,11,9,4],[7,0,7,27],[7,4,28,14],[3,10,26,7]];
+
+console.log(x[2][0]);
+console.log(x [2] [1]);
+console.log(x[2][0]);
+
 
